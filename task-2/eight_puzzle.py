71671a0fd288bf45f619a6595e3e3f713dd119e3
mmukttp <99861883+mmukttp@users.noreply.github.com>
2023-09-11 11:23:13 +0700

    Update eight_puzzle.py

--- task-2/eight_puzzle.py
+++ task-2/eight_puzzle.py
@@ -49,6 +49,21 @@ def h1(s):
     return res
 
 def h3(s):
-    # implement this function
+    goal = (1, 2, 3, 4, 5, 6, 7, 8, 0)
+    pos = {1:[0,0], 2:[1,0], 3:[2,0], 4:[0,1], 5:[1,1], 6:[2,1], 7:[0,2], 8:[1,2], 0:[2,2]}
+    
     board, _, _ = s
-    return 0
+    col_dis = 0
+    row_dis = 0
+    
+    for i in range(9):
+        if goal[i] != board[i]:
+            if pos[goal[i]][0] != pos[board[i]][0]:
+                row_dis += 1
+
+        if goal[i] != board[i]:
+            if pos[goal[i]][1] != pos[board[i]][1]:
+                col_dis += 1
+    
+    total = row_dis + col_dis
+    return total
